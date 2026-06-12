09549f96c46101020518f546ea992fa05b6f692a
Luis Gonzalo Morelli <gonzy.morelli@gmail.com>
2026-06-12 09:19:50 -0300

    Agregar archivos JavaScript para ejemplos de arreglos y palabras reservadas

---
+++ 01-fundamentos/assets/js/arreglos.js
@@ -0,0 +1,20 @@
+// const arr = new Array(10);
+// const arr2 = []
+// console.log(arr);
+
+let videoGames = ['Mario 3', 'Megaman', 'Chrono Trigger'];
+console.log({ videoGames });
+console.log(videoGames[0]);
+
+let arregloCosas = [
+    true,
+    123,
+    'Fernando',
+    1 + 2,
+    function () { },
+    () => { },
+    { a: 1 },
+    ['X', 'Megaman', 'Zero', 'Dr. Light', ['Dr. Willy', 'Woodman']]
+]
+
+console.log(arregloCosas[7][3]);
---
+++ 01-fundamentos/assets/js/arreglos2.js
@@ -0,0 +1,24 @@
+let juegos = ['Zelda', 'Mario', 'Metroid', 'Castlevania'];
+console.log(juegos.length);
+
+let primero = juegos[0];
+let ultimo = juegos[juegos.length - 1];
+
+console.log({ primero, ultimo });
+
+juegos.forEach((elemento , indice , arr ) => {
+  console.log({ elemento, indice, arr });
+})
+
+let nuevaLongitud = juegos.push('F-Zero');
+console.log({  nuevaLongitud , juegos });
+
+nuevaLongitud = juegos.unshift('Fire Emblem');
+console.log({ nuevaLongitud, juegos });
+
+let juegoBorrado = juegos.pop();
+console.log({ juegoBorrado, juegos });
+
+let pos = 1;
+let juegosBorrados = juegos.splice(pos, 2);
+console.log({ juegosBorrados, juegos });  
