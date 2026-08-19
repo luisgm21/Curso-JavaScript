f6e3e660430dbaca82d70d5db68ea69c82321c99
Luis Gonzalo Morelli <gonzy.morelli@gmail.com>
2026-08-18 21:17:58 -0300

    Agregar clase Persona en constructores-multiples.js y actualizar referencia en index.html

---
+++ 01-fundamentos/assets/js/classes/constructores-multiples.js
@@ -0,0 +1,33 @@
+class Persona {
+
+  static porObjeto({ nombre, apellido, pais }) {
+    return new Persona(nombre, apellido, pais);
+  }
+
+  constructor(nombre, apellido,pais) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.pais = pais;
+  }
+
+  getInfo() {
+    return `${this.nombre} ${this.apellido} de ${this.pais}`;
+  }
+
+}
+
+const nombre1   = "Melissa",
+      apellido1 = "Ramos",
+      pais1     = "México";
+
+const fher = {
+  nombre: "Fernando",
+  apellido: "Herrera",
+  pais: "Argentina"
+};
+
+const persona1 = new Persona(nombre1, apellido1, pais1);
+const persona2 = Persona.porObjeto(fher);
+
+console.log(persona1.getInfo());
+console.log(persona2.getInfo());
